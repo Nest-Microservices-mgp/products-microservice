dfb7dfead21ff5ec4182e3d3a8cc0b1d5d9691d7
mgarces <manugp1994@gmail.com>
2025-08-02 11:09:41 +0200

    Video82

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,25 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().default(3000),
     DATABASE_URL: joi.string().uri().required(),
+    NATS_SERVERS: joi
+      .array()
+      .items(joi.string().uri())
+      .required()
+      .default(['nats://localhost:4222']),
   })
   .unknown();
 
-const { error, value: envVars } = envVarsSchema.validate(process.env);
+const { error, value: envVars } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -22,4 +31,5 @@ if (error) {
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 };
