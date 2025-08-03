c513f260885c55a1b4fa50bd220b9f3cf25142f4
mgarces <manugp1994@gmail.com>
2025-08-03 10:47:33 +0200

    Video95

--- prisma/migrations/20250802154734_init/migration.sql
+++ prisma/migrations/20250802154734_init/migration.sql
@@ -3,9 +3,13 @@ CREATE TABLE "Product" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "price" REAL NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- CreateIndex
+CREATE INDEX "Product_available_idx" ON "Product"("available");
